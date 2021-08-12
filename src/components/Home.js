7514ab3bc6c316b6f8db44f3af191e5eeb5ff33d
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-08-12 00:03:40 -0700

    updated Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from './NavBar';
 
 export default function home(){
     return(
-        <div className = "home">
-            <Link className="home-link" to="/nasaphoto"><span>The Limit Is The Sky! <br/>A.P.O.D <br/> Astronomy Picture Of The Day.</span></Link>
+      <>  
+        <NavBar/>
+        <div >
+            <h1>Astronomy Picture Of the Day - APOD</h1>
+            <p>Can you believe that one of the most popular websites at NASA is the Astronomy Picture of the Day? In fact, this website is one of the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video. </p>
+            <p>I hope you have fun exploring the universe!</p>
         </div>
-        
+        <div>
+          <a
+						className='about-anchor'
+						target='_blank'
+						rel='noopener noreferrer'
+						href='https://github.com/PriscilaMonteiro'>
+						GitHub
+					</a>
+        </div>
+      </> 
         
 
     )
